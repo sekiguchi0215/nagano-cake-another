1c8ba346696f86f31abd36ba22f378e269026c81
vagrant <dsphere5296@gmail.com>
2021-03-04 09:37:23 +0000

    コマンドを利用してpublic/homes_controller.rbの生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/about'
+  end
 	root "public/homes#top"
+
+	devise_for :members, controllers: {
+		sessions:       "members/sessions",
+		passwords:      "members/passwords",
+		registrations:  "members/registrations"
+	}
 end
