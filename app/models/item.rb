33250b220c1ad25d50cf2e741aa561421d4a6f84
vagrant <dsphere5296@gmail.com>
2021-03-04 19:23:35 +0000

    models/item.rbの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+	has_many :cart_items
+	has_many :order_items
+	belongs_to :genre
+
+	attachment :image
+
+	enum sale_status: { 販売中: 0, 販売停止: 1 }
+
+	validates :item_name, presence: true
+	validates :explanation, presence: true
+	validates :unit_price_without_tax, presence: true
 end
