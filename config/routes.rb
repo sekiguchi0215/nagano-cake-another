94fd8d5e9a4c97caa7086d74755607256c912495
vagrant <dsphere5296@gmail.com>
2021-03-04 17:07:42 +0000

    routes.rbの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,44 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'homes/about'
-  end
-	root "public/homes#top"
+  	root "public/homes#top"
 
 	devise_for :members, controllers: {
 		sessions:       "members/sessions",
 		passwords:      "members/passwords",
 		registrations:  "members/registrations"
 	}
+
+	devise_for :admins, controllers: {
+		sessions:       "admins/sessions",
+		passwords:      "admins/passwords",
+		registrations:  "admins/registrations"
+	}
+
+	namespace :admin do
+		get "/admins" => "admins#top"
+		resources :items
+		resources :genres
+		resources :members
+		resources :orders, only: [:index, :show, :update]
+		resources :order_items, only: [:update]
+	end
+
+	namespace :public do
+		get "/about" => "homes#about"
+		delete "/cart_items/destroy_all" => "cart_items#destroy_all"
+		resoureces :items, only: [:index, :show, :new] do
+			get :search, on: :collection
+		end
+		resources :cart_items
+		resources :orders, only: [:new, :create, :index, :show]
+		post "/orders/session" => "orders#session_create"
+		get "/orders/confirm" => "ordres#confirm"
+		post "/orders/confirm" => "ordres#confirm"
+		get "/orders/thanks" => "orders#thanks"
+		
+		resource  :members, only: [:show, :edit, :update]
+		patch "/members/withdrawal" => "members#destroy"
+		get "/members/withdrawal" => "members#withdrawal"
+		
+		resources :addresses, only: [:index, :edit, :destroy, :create, :update]
+	end
 end
