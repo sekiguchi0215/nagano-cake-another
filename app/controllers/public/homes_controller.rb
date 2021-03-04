b16979d14728081aff8c755497ea5ebf707a126d
vagrant <dsphere5296@gmail.com>
2021-03-04 10:37:52 +0000

    public/homesのviewページの記述

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
+  	@genres = Genre.where(valid_invalid_status: 0)
+  	@items = Item.limit(8).offset(4)
+  	# "limit"は、指定したレコード数だけ取得する（最大取得行数）。
+  	# "offset"は、指定した位置からレコードを取得する（取得開始位置）。
   end
 
   def about
