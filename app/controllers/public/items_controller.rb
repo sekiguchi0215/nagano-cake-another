755053caab4cc1dc26aaa42e955c318f91ebfc4c
vagrant <dsphere5296@gmail.com>
2021-03-04 19:20:07 +0000

    public/items_controller.rbの記述

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,19 @@
 class Public::ItemsController < ApplicationController
   def index
+  	@items = Item.where(sale_status: 0).page(params[:page]).per(8)
+  	@quantity = Item.count
+  	@genres = Genre.where(valid_invalid_status: 0)
   end
 
   def show
+  	@item = Item.find(params[:id])
+  	@cart_item = CartItem.new
+  	@genres = Genre.where(valid_invalid_status: 0)
   end
+
+  def search
+  	@items = Item.where(genre_id: params[:format]).page(params[:page]).per(8)
+  	@quantity = Item.where(genre_id: params[:format]).count
+  	@genres = Genre.where(valid_invalid_status: 0)
+  	render "index"
 end
